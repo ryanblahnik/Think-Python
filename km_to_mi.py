ba875e385395f1fd4fb4129f8cd76ad07c80accf
Ryan <ryanblahnik@gmail.com>
2016-05-22 17:36:11 -0500

    Add files via upload
    005

---
+++ km_to_mi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+kminmi = 1.61
+print('How many kilometers?')
+km = input()
+print('')
+print(str(kminmi) + ' kilometers in 1 mile...')
+print('')
+mi = kminmi * int(km)
+print(str(mi) + ' miles in ' + str(km) + ' kilometers. Thank you!')
+print('')
+print('.')
+print('.')
+print('.')
+print('')
+print('How many kilometers?')
+
